fec14ac7684ae5258f46fdc8b9a110eca4c3e6d5
jgomezdans <gomezdansj@gmail.com>
2010-06-03 19:52:16 +0100

    WSDL services

--- scripts/get_af.py
+++ scripts/get_af.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
+"""
+    Stuff about getting things from the web
+"""
+from modisficator import wsdl_modis
 
 
-def GetActiveFires ( fname, fire_thresh=8 ):
+def get_active_fires ( fname, fire_thresh=8 ):
     """
     Get active fires from MOD14A1.005 dataset. Returns a list of longitudes, latitudes, and day of fire
     """
@@ -47,6 +51,49 @@ def GetActiveFires ( fname, fire_thresh=8 ):
     fout.close()
     return return_struct
 
+
+
+
+def get_nbar_rho ( lon, lat, date, t_window = 42 ):
+    """
+    Get a time series of NBAR reflecances from the MODIS webservice
+    """
+    year = int(date[1:5])
+    doy =  int(date[5:])
+    return_dict = {}
+    day_start = doy - t_window
+    day_end = doy + t_window
+    x_pixels = 0
+    y_pixels = 0
+    # First, get QA from MCD43A2 product
+    product = "MCD43A2"
+    for layer in ["BRDF_Albedo_Quality", "BRDF_Albedo_Band_Quality" ] :
+        print layer
+        ( dates, datas ) = wsdl_modis.wsdl_get_snapshot( lon, lat, \
+                product, layer, year, \
+                day_start, day_end, x_pixels, y_pixels )
+        return_dict[ layer ] = datas
+    # Now, NBAR refl from MCD43A4 product
+    product = "MCD43A4"
+    for nlayer in xrange(1, 8):
+        layer = "Nadir_Reflectance_Band%d" % nlayer
+        print layer
+        ( dates, datas ) = wsdl_modis.wsdl_get_snapshot( lon, lat, \
+                product, layer, year, \
+                day_start, day_end, x_pixels, y_pixels )
+        return_dict[ layer ] = datas
+    return_dict[ 'dates' ] = dates
+    return return_dict
+    
 if __name__ == "__main__":
-    queimas = GetActiveFires ( \
-        "./test/MOD14A1.A2003137.h09v07.005.2007319180038.hdf" )
+    import pylab, numpy, time
+    #queimas = get_active_fires ( \
+        #"./test/MOD14A1.A2003137.h09v07.005.2007319180038.hdf" )
+    dates = []
+    queimas = numpy.loadtxt ( "./test/MOD14A1.A2003137.h09v07.005.2007319180038_LonLat.txt", \
+        delimiter=";", usecols=(1,2,3), converters={1: dates.append})[:, 1:]
+    ( lon, lat ) = queimas[200]
+    date = dates[200].lstrip().rstrip()
+    print "Starting slurrrping", time.asctime()
+    datos = get_nbar_rho ( lon, lat, date, t_window = 42 )
+    print "Finished slurrrping", time.asctime()
